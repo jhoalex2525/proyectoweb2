929dafe565e64eece53fc2b89938b4ad25053479
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-10-06 09:38:44 -0500

    Se crea formulario de la vista registro productos

--- app/Views/registroProductos.php
+++ app/Views/registroProductos.php
@@ -1,12 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo (base_url('public/styles/styles.css')) ?>">
+    <title>ANIMALANDIA</title>
 </head>
+
 <body>
-    <h1>Hola mundo</h1>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark principalbackground">
+            <div class="container-fluid">
+                <a class="navbar-brand principalfont" href="#">
+                    <i class="fas fa-cat"></i>
+                    ANIMALANDIA
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="<?= site_url('/') ?>">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= site_url('/productos/registro') ?>">Registro Productos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-6">
+                    <form method="" action="<?= ?>" class="mt-4">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Producto:</label>
+                            <input type="text" class="form-control" id="producto" placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Fotografía:</label>
+                            <input type="text" class="form-control" id="photo" placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Precio Unidad:</label>
+                            <input type="text" class="form-control" id="price" placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <textarea class="form-control" name="" id="description" cols="30" rows="5" placeholder="Descripción"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Tipo de animal:</label>
+                            <select name="" class="form-control" id="animaltype">
+                                <option value="0" selected disabled>Seleccione un animal</option>
+                                <option value="1">Perro</option>
+                                <option value="2">Gato</option>
+                                <option value="3">Ave</option>
+                                <option value="4">Caballo</option>
+                                <option value="5">Reptil</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary w-100">enviar</button>
+                        </div>                        
+                    </form>
+                </div>
+                <div class="col-6 d-flex align-items-center">
+                    <img class="img-fluid" src="<?= base_url('public/img/imagen4.jpg') ?>" alt="">
+                </div>
+            </div>            
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fb8e0c1491.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
